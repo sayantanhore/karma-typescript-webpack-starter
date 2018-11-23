0f537f5ba9637c6931caee030fcedbc811081009
Sayantan Hore <sayantan.hore.86@gmail.com>
2018-11-24 01:57:01 +0200

    Test coverage report generation with istanbul added

--- karma.conf.js
+++ karma.conf.js
@@ -6,18 +6,24 @@ module.exports = function(config) {
     exclude: ['node_modules/**/*'],
     frameworks: ['jasmine'],
     preprocessors: {
-      '**/*.ts': ['webpack']
+      '**/!(*.spec).ts': ['webpack', 'coverage'],
+      '**/*.spec.ts': ['webpack']
     },
     webpack: webpackTestConfig,
     plugins: [
       'karma-jasmine', 
       'karma-chrome-launcher',
-      'karma-webpack'
+      'karma-webpack',
+      'karma-coverage',
+      'karma-jasmine-html-reporter'
     ],
     mime: {
       'text/x-typescript': ['ts', 'tsx']
     },
     browsers: ['Chrome'],
-    reporters: ['progress']
+    client: {
+      clearContext: false
+    },
+    reporters: ['kjhtml', 'coverage']
   })
 }
